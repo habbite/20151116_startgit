e09f60cb6b6bcb3024796658539df78de5a27232
doni <289265458@qq.com>
2016-11-16 17:40:07 +0800

    add a anim project[fixer]doni

---
+++ animation/animation/library/src/main/java/shimmer/anim/com/library/ShimmerTextView.java
@@ -0,0 +1,30 @@
+package shimmer.anim.com.library;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/**
+ * Created by Yuriy on 2016/11/16.
+ */
+public class ShimmerTextView extends TextView {
+
+
+    /***************************************/
+    public ShimmerTextView(Context context) {
+        super(context);
+    }
+
+    public ShimmerTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ShimmerTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+    /***************************************/
+
+
+
+
+}
